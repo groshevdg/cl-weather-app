268b455cdd125e2b80140e41849ddab93f33808a
groshevdg <groshevdg@gmail.com>
2021-08-24 13:26:17 +0300

    Added icons to immutable params, set up loading state

---
+++ lib/common/app_assets.dart
@@ -0,0 +1,6 @@
+class AppAssets {
+  static const rain = 'res/assets/rain.svg';
+  static const sunrise = 'res/assets/sunrise.svg';
+  static const sunset = 'res/assets/sunset.svg';
+  static const wind = 'res/assets/wind.svg';
+}
