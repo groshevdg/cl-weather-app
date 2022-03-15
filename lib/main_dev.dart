32628ccf38c01c28cc8174fd54aef707dc7551d2
groshevdg <groshevdg@gmail.com>
2022-03-15 19:02:46 +0300

    fixed imports

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,5 +1,5 @@
 import 'package:cl_weather_app/common/debug_bloc_observer.dart';
-import 'package:cl_weather_app/common/env/build_types.dart';
+import 'package:cl_weather_app/common/env/build_type.dart';
 import 'package:cl_weather_app/common/env/config.dart';
 import 'package:cl_weather_app/common/env/debug_options.dart';
 import 'package:cl_weather_app/common/env/environment.dart';
